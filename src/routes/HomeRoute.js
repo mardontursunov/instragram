2ee25ae2eb3754185b886b6b19cec85ab974fdc8
Mardon <tursunovmardon7@gmail.com>
2021-09-24 16:41:44 +0500

    added public module

--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: "Instagram"
+    })
 })
 
 module.exports = {
